e4a5c0ba6596bc8a63fb8341b1e03ea1938475ce
Askat0424 <askatasanbekov0424@gmail.com>
2023-04-29 14:58:58 +0600

    Initial commit

---
+++ app/src/main/java/com/example/asanbekov_6_2/Photo.kt
@@ -0,0 +1,6 @@
+package com.example.asanbekov_6_2
+
+data class Photo (
+    var photo: String,
+
+        )
---
+++ app/src/main/java/com/example/asanbekov_6_2/SelectedActivity.kt
@@ -0,0 +1,16 @@
+package com.example.asanbekov_6_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.asanbekov_6_2.databinding.ActivitySelectedBinding
+
+class SelectedActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySelectedBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySelectedBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
